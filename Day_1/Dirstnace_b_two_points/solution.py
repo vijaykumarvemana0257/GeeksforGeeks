102ffe1e813f24ab0d9f5de50a0bf4d169830ea5
vijaykumarvemana0257 <vijaykumarvemana0257@gmail.com>
2023-11-11 14:27:05 -0500

    basics

---
+++ Day_1/Dirstnace_b_two_points/solution.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+import math
+class Solution:
+    def distance(self, x1, y1, x2, y2):
+        # Code here
+        return int(round(math.sqrt(((x2-x1)**2)+((y2-y1)**2))))
+
---
+++ Day_1/sum_of_AP/solution.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    def sum_of_ap(self, n, a, d):
+        # Code here
+        o=(n/2)*((2*a)+((n-1)*d))
+        return int(o)
+
