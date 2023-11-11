c17f7ef4a73e9f32f3aa58048a404e76a2372224
vijaykumarvemana0257 <vijaykumarvemana0257@gmail.com>
2023-11-11 14:45:24 -0500

    basics

--- Day_1/odd_even/solution.py
+++
@@ -1,9 +0,0 @@
-#User function Template for python3
-class Solution:
-    def oddEven (ob,N):
-        # code here 
-        if N %2 == 0:
-            return "even"
-        else:
-            return "odd"
-
